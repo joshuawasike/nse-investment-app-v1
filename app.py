cb3a74f9343ee3ccdcd47daaa51db63cd36edcd7
joshuawasike <joshua.wasike2000@gmail.com>
2026-06-01 23:29:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -269,5 +269,11 @@ def index():
         return f"APP ERROR: {str(e)}"
 
 
+import os
+
+# =========================================================
+# RUN (RENDER SAFE)
+# =========================================================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
